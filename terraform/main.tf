f89b570a53f786603c99ba946db5d04c52fcfcbf
ludovic.bondon <ludovic.bondon@hub2.io>
2025-12-11 14:54:25 +0400

    Refactor main.tf to use variables instead of hardcoded placeholders & Add terraform.tfvars.example

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,35 @@ provider "azurerm" {
   features {}
 }
 
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region"
+  type        = string
+}
+
+variable "acr_name" {
+  description = "The name of the Azure Container Registry"
+  type        = string
+}
+
+variable "aca_env_name" {
+  description = "The name of the Azure Container Apps Environment"
+  type        = string
+}
+
+variable "gitlab_runner_principal_id" {
+  description = "The Principal ID of the GitLab Runner's Managed Identity"
+  type        = string
+}
+
 # Example Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "rg-automation-framework"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Call the Container Apps Module
@@ -16,14 +41,12 @@ module "container_apps" {
   resource_group_id   = azurerm_resource_group.example.id
   location            = azurerm_resource_group.example.location
 
-  acr_name     = "myautomationacr" # Must be globally unique and alphanumeric
-  aca_env_name = "aca-env-automation"
+  acr_name     = var.acr_name
+  aca_env_name = var.aca_env_name
 
-  # REPLACE THIS with your GitLab Runner's Managed Identity Principal ID
-  gitlab_runner_principal_id = "00000000-0000-0000-0000-000000000000"
+  gitlab_runner_principal_id = var.gitlab_runner_principal_id
 }
 
 output "automation_jobs_identity_id" {
   value = module.container_apps.jobs_identity_id
 }
-
